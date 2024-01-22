f018babf98f329d6a22ba5072919e47fc3a42e72
Vivek <ojhavivek24@gmail.com>
2024-01-22 06:31:13 +0000

    Server.js Initial file configuration

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express()
+
+app.use(express.json())
+app.use("/public",express.static(path.join(path.join(__dirname,"public"))))
+
+app.get("/",(req,res)=>{
+    res.send(path.join(__dirname,"public"))
+})
+
+app.listen(3500,()=>{
+    console.log("Server is running");
+})
